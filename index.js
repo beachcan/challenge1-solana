8ac3130432110bf71a4be88ebe7390a20aebf031
beachcan <71321301+beachcan@users.noreply.github.com>
2022-08-10 01:59:12 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// Import Solana web3 functinalities
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL
+} = require("@solana/web3.js");
+
+// Specify airdrop address
+const publicKey = 'pra3K9s6HMCvNUKfXSkCcFq7MWX5zqoc5braHpQRHYR'
+
+// Connect to the Devnet
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+console.log("Wallet address to be airdropped:", publicKey); 
+
+// Get the wallet balance from given public key
+const getWalletBalance = async () => {
+    try {
+        // Connect to the Devnet
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const walletBalance = await connection.getBalance(
+            new PublicKey(publicKey)
+        );
+        console.log(`Wallet balance: ${parseInt(walletBalance) / LAMPORTS_PER_SOL} SOL`);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const airDropSol = async () => {
+    try {
+        // Connect to the Devnet
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+        // Request airdrop of 2 SOL to the wallet
+        console.log("Airdropping some SOL to my wallet!");
+        const fromAirDropSignature = await connection.requestAirdrop(
+            new PublicKey(publicKey),
+            2 * LAMPORTS_PER_SOL
+    
+        );
+        await connection.confirmTransaction(fromAirDropSignature);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+// Show the wallet balance before and after airdropping SOL
+const mainFunction = async () => {
+    await getWalletBalance();
+    await airDropSol();
+    await getWalletBalance();
+}
+
+mainFunction();
